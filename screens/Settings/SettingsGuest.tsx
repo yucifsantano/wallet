e245d88660b06ffec8fdbd69445c188c7c5454aa
Yucif Santano <ysantano@ciericorp.com>
2022-02-03 12:00:21 -0600

    Primera pantalla de perfil, termiada.

--- screens/Settings/SettingsGuest.tsx
+++ screens/Settings/SettingsGuest.tsx
@@ -1,19 +1,62 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { StyleSheet, View,  ScrollView, Text, Image } from "react-native";
+import { Button } from "react-native-elements";
 
 export default function SettingsGuest() {
     return (
-        <View style={styles.container}>
-          <Text>Settings Guest</Text>
+      <ScrollView centerContent={true} style={styles.viewBody}>
+        <Image
+          style={styles.image}
+          source={require("../../assets/img/user-guest.jpg")}
+          resizeMode="contain"
+        />
+        <Text style={styles.title}>Consulta tu perfíl</Text>
+        <Text style={styles.description}>
+          ¿Cómo describirías tu mejor restaurante? Busca y visualiza los mejores 
+          restaurantes de una forma sencilla, vota cual te ha gustado más y 
+          comenta como ha sido tu experiencia.
+        </Text>
+        <View style={styles.viewBtn}>
+          <Button 
+            title="Ver tu pefíl"
+            buttonStyle={styles.btnStyle}
+            containerStyle={styles.btnContainer}
+            onPress={() => console.log("Click!!")}
+          />
         </View>
-      );
+      </ScrollView>
+    );
     
 }
 const styles = StyleSheet.create({
-  container: {
+  viewBody: {
+    backgroundColor: "#f9f7f7",
+  },
+  image: {
+    height: 300,
+    width: "100%",
+    marginBottom: 40,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 19,
+    marginBottom: 10,
+    textAlign: "center",
+  },
+  description: {
+    textAlign: "center",
+    marginBottom: 20,
+    marginLeft: 25,
+    marginRight: 25,
+  },
+  viewBtn: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#f9f7f7",
+  },
+  btnStyle: {
+    backgroundColor: "#00a680"
+  },
+  btnContainer: {
+    width: "70%"
   },
 });
--- screens/Settings/SettingsScreen.tsx
+++ screens/Settings/SettingsScreen.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-
+import Loading from "../../components/Loading";
 import SettingsGuest from "./SettingsGuest";
 import SettingsUser from "./SettingsUser";
 
 export default function SettingsScreen() {
   const userLogg = false;
+
+  //if (!userLogg) return <Loading isVisible={true} text="Cargando..." />
 
   return userLogg ? <SettingsUser/> : <SettingsGuest/>;
 }

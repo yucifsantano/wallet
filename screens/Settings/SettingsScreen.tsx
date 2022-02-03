dfef53be88beebbc8d1938c27c3b0be92f16bce1
Yucif Santano <ysantano@ciericorp.com>
2022-02-03 10:36:37 -0600

    Inicio de componentes globales

--- screens/Settings/SettingsScreen.tsx
+++ screens/Settings/SettingsScreen.tsx
@@ -1,31 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 import SettingsGuest from "./SettingsGuest";
 import SettingsUser from "./SettingsUser";
 
 export default function SettingsScreen() {
-  const [login, setLogin] = useState(null);
+  const userLogg = false;
 
-  /*
-  useEffect(() => {
-    setLogin(false);
-  }, []);
-  */
-
-  //if (login=== null) return(<text>Cargando...</text>);
-
-  return (
-      <View style={styles.container}>
-        <Text>Settings Screen</Text>
-      </View>
-    );
+  return userLogg ? <SettingsUser/> : <SettingsGuest/>;
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#f9f7f7",
-  },
-});
